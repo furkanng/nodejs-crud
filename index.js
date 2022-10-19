d26b3e8a167b1e2390ee04af39b8f003e4ff430c
furkanng <furkanguzelgorur@gmail.com>
2022-10-19 15:38:30 +0300

    proje tamamlandı

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ const app = express();
 const PORT = 5000;
 
 const middleware = function (req, res, next) {
-  console.log("Yeni bir istek geldi");
+  console.log("İstek geldi");
   next();
 };
 app.get("/hello", middleware, (req, res) => {
-  console.log("merhaba get istegi attınız");
+  console.log("hello get istegi attınız");
   res.json("hello get rest api");
 });
 
 app.post("/hello", middleware, (req, res) => {
-  console.log("merhaba post istegi attınız");
+  console.log("hello post istegi attınız");
   res.json("hello post rest api");
 });
 
 app.put("/hello", middleware, (req, res) => {
-  console.log("Merhaba put istegi attınız");
+  console.log("hello put istegi attınız");
   res.json("hello put rest api");
 });
 
 app.delete("/hello", middleware, (req, res) => {
-  console.log("Merhaba delete istegi attınız");
+  console.log("hello delete istegi attınız");
   res.json("hello delete rest api");
 });
 
