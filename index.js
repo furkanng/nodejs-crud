fe6128b215111adda4f373683c4b014664a89d21
furkanng <furkanguzelgorur@gmail.com>
2022-10-19 14:49:10 +0300

    degisiklik yapıldı

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const  express = require('express');
-const bodyParser =require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = 5000
+const PORT = 5000;
 
+const middleware = function (req, res, next) {
+  console.log("Yeni bir istek geldi");
+  next();
+};
+app.get("/hello", middleware, (req, res) => {
+  console.log("merhaba get istegi attınız");
+  res.json("hello get rest api");
+});
 
-app.listen(PORT,()=>{
-    console.log("listening",PORT);
-})
+app.post("/hello", middleware, (req, res) => {
+  console.log("merhaba post istegi attınız");
+  res.json("hello post rest api");
+});
+
+app.put("/hello", middleware, (req, res) => {
+  console.log("Merhaba put istegi attınız");
+  res.json("hello put rest api");
+});
+
+app.delete("/hello", middleware, (req, res) => {
+  console.log("Merhaba delete istegi attınız");
+  res.json("hello delete rest api");
+});
+
+app.listen(PORT, () => {
+  console.log("listening", PORT);
+});
